0f36dfb22f6fa354fec69ed19cabe5370d081244
AlagappanRa <96157054+AlagappanRa@users.noreply.github.com>
2023-09-26 17:30:12 +0800

    Revert "Merge pull request #3 from AlagappanRa/tutorial-adding-command"
    This reverts commit 1e8191ec2f21a015c7abb7eeddd640881e18eee4, reversing
    changes made to 8a14e119544157f4982a4b74e44c18284ff1c069.
    
    Yes.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -28,9 +28,6 @@ public class LogicManager implements Logic {
     private final Storage storage;
     private final AddressBookParser addressBookParser;
 
-    /**
-     * Creates a {@code LogicManager} with the given {@code Model} and {@code Storage}.
-     */
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.storage = storage;
