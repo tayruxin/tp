a7f5dc19b2d23b7200f03ac3668a37e7cf571b95
papataco14 <rithikvj@gmail.com>
2023-09-27 00:26:20 +0800

    Revert "Fixed coding standard violations"
    This reverts commit 993e404917960dbe405f88a6df3179bcab0007fd.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -31,7 +31,7 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Remark remark,  Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
@@ -57,9 +57,7 @@ public class Person {
         return address;
     }
 
-    public Remark getRemark() {
-        return remark;
-    }
+    public Remark getRemark() { return remark; }
 
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
