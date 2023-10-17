921786e3c9c4b8fa67842dd54e383c6166a82ed0
papataco14 <rithikvj@gmail.com>
2023-10-17 20:18:54 +0800

    Change deadline to LocalDate
    - Changed the deadline field in the deadline class from a String to a java LocalDate object for better date manipulation

--- src/main/java/seedu/address/model/company/Deadline.java
+++ src/main/java/seedu/address/model/company/Deadline.java
@@ -3,6 +3,9 @@ package seedu.address.model.company;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Represents a Company's deadline in the address book.
@@ -14,7 +17,9 @@ public class Deadline {
             "Please enter a valid deadline in the format DD-MM-YYYY";
     public static final String VALIDATION_REGEX = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19[0-9]{2}|2[0-9]{3})$";
 
-    public final String value;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public final LocalDate value;
 
 
     /**
@@ -25,19 +30,29 @@ public class Deadline {
     public Deadline(String deadline) {
         requireNonNull(deadline);
         checkArgument(isValidDeadline(deadline), MESSAGE_CONSTRAINTS);
-        value = deadline;
+        this.value = LocalDate.parse(deadline, FORMATTER);
     }
 
     /**
      * Returns true if a given string is a valid deadline.
      */
     public static boolean isValidDeadline(String test) {
-        return test.matches(VALIDATION_REGEX);
+        if (!test.matches(VALIDATION_REGEX)) {
+            return false;
+        }
+
+        try {
+            LocalDate.parse(test, FORMATTER);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
+
 
     @Override
     public String toString() {
-        return value;
+        return value.format(FORMATTER);
     }
 
     @Override
