3e6ad199afcb8a35afc503bc1e8d59090dd2b2bc
tayruxin <tayruxin00@gmail.com>
2023-10-18 13:10:11 +0800

    Update UG-View function

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -11,7 +11,6 @@ import seedu.address.model.company.Company;
  * Container for user visible messages.
  */
 public class Messages {
-
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_COMPANY_DISPLAYED_INDEX = "The company index provided is invalid";
