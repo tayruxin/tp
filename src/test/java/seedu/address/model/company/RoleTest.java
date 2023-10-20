9c93545e8823a9d9e6bb5a3dc7bcfe0708b95b08
GlendaChong <glendachong04@gmail.com>
2023-10-20 14:56:20 +0800

    Add test cases for Role field

---
+++ src/test/java/seedu/address/model/company/RoleTest.java
@@ -0,0 +1,59 @@
+package seedu.address.model.company;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class RoleTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Role(null));
+    }
+
+    @Test
+    public void constructor_invalidRole_throwsIllegalArgumentException() {
+        String invalidRole = "";
+        assertThrows(IllegalArgumentException.class, () -> new Role(invalidRole));
+    }
+
+    @Test
+    public void isValidRole() {
+        // null role
+        assertThrows(NullPointerException.class, () -> Role.isValidRole(null));
+
+        // invalid role
+        assertFalse(Role.isValidRole("")); // empty string
+        assertFalse(Role.isValidRole(" ")); // spaces only
+        assertFalse(Role.isValidRole("^")); // only non-alphanumeric characters
+        assertFalse(Role.isValidRole("software*engineer")); // contains non-alphanumeric characters
+
+        // valid role
+        assertTrue(Role.isValidRole("software engineer")); // alphabets only
+        assertTrue(Role.isValidRole("12345")); // numbers only
+        assertTrue(Role.isValidRole("web3 developer")); // alphanumeric characters
+        assertTrue(Role.isValidRole("Software Engineer")); // with capital letters
+        assertTrue(Role.isValidRole("Machine Learning and Artificial Intelligence Engineer")); // long roles
+    }
+
+    @Test
+    public void equals() {
+        Role role = new Role("Valid Role");
+
+        // same values -> returns true
+        assertTrue(role.equals(new Role("Valid Role")));
+
+        // same object -> returns true
+        assertTrue(role.equals(role));
+
+        // null -> returns false
+        assertFalse(role.equals(null));
+
+        // different types -> returns false
+        assertFalse(role.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(role.equals(new Role("Other Valid Role")));
+    }
+}
