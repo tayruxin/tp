20b4bf5b2270130659dac80fbb848efdf3c09116
papataco14 <rithikvj@gmail.com>
2023-10-25 21:39:35 +0800

    Update find command
    - Modify find command to now
    include substring searches
    - Add logging statements to find
    command

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,23 +2,29 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.company.NameContainsKeywordsPredicate;
 
 /**
- * Finds and lists all companies in address book whose name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Finds and lists all companies in the address book whose name matches the criteria
+ * specified in the provided predicate.
+ *
+ * For detailed matching behavior, see {@link NameContainsKeywordsPredicate}.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
+    private static final Logger logger = LogsCenter.getLogger(FindCommand.class);
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all companies whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " micro ";
 
     private final NameContainsKeywordsPredicate predicate;
 
@@ -29,6 +35,7 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        logger.info("Executing FindCommand with predicate: " + predicate);
         model.updateFilteredCompanyList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_COMPANIES_LISTED_OVERVIEW, model.getFilteredCompanyList().size()));
