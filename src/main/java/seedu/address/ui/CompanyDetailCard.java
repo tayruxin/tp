7ce6c7e5dc74fe17cf385a468ec9208de44bc4bd
alientian <e1062165@u.nus.edu>
2023-10-26 23:40:19 +0800

    Add assertions

--- src/main/java/seedu/address/ui/CompanyDetailCard.java
+++ src/main/java/seedu/address/ui/CompanyDetailCard.java
@@ -48,6 +48,7 @@ public class CompanyDetailCard extends UiPart<Region> {
      */
     public CompanyDetailCard(Company company) {
         super(FXML);
+        assert company != null;
         this.company = company;
         name.setText(company.getName().fullName);
         phone.setText("Phone: " + company.getPhone().value);
@@ -61,6 +62,7 @@ public class CompanyDetailCard extends UiPart<Region> {
     }
 
     private void priorityFlowPane(Company company) {
+        assert company != null;
         if (!company.getPriority().priority.equals("NONE")) {
             FlowPane priorityPane = new FlowPane();
 
