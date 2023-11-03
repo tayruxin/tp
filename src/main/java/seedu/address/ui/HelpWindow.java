7dafc6d3c7d1c3747830e885d6aaa23b876a90cc
AlagappanRa <96157054+AlagappanRa@users.noreply.github.com>
2023-11-03 08:51:25 +0800

    Implement Help Command
    Implement Help Command

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -1,12 +1,15 @@
 package seedu.address.ui;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 
@@ -14,18 +17,19 @@ import seedu.address.commons.core.LogsCenter;
  * Controller for a help page
  */
 public class HelpWindow extends UiPart<Stage> {
-
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String USER_GUIDE_URL = "https://ay2324s1-cs2103t-t17-2.github.io/tp/UserGuide.html";
+    public static final String MESSAGE_FAILURE = "Failed to open help page. "
+            + "Please open LinkMeIn's user guide in your browser at "
+            + USER_GUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
     @FXML
-    private Button copyButton;
-
+    private Button accessBrowserButton;
     @FXML
-    private Label helpMessage;
+    private WebView webView;
+    private WebEngine webEngine;
 
     /**
      * Creates a new HelpWindow.
@@ -34,7 +38,8 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
+        webEngine = webView.getEngine();
+        webEngine.load(USER_GUIDE_URL);
     }
 
     /**
@@ -42,6 +47,20 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow() {
         this(new Stage());
+    }
+
+    /**
+     * Opens the user guide in the default browser within the application.
+     */
+    @FXML
+    private void useBrowser() {
+        try {
+            Desktop.getDesktop().browse(new URL(USER_GUIDE_URL).toURI());
+            hide();
+        } catch (IOException | URISyntaxException e) {
+            hide();
+            MainWindow.displayResult(MESSAGE_FAILURE);
+        }
     }
 
     /**
@@ -87,16 +106,5 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public void focus() {
         getRoot().requestFocus();
-    }
-
-    /**
-     * Copies the URL to the user guide to the clipboard.
-     */
-    @FXML
-    private void copyUrl() {
-        final Clipboard clipboard = Clipboard.getSystemClipboard();
-        final ClipboardContent url = new ClipboardContent();
-        url.putString(USERGUIDE_URL);
-        clipboard.setContent(url);
     }
 }
