cedd02f4df0f61f515d1cc27c41665c3fd5936fb
vijay <rithikvj@gmail.com>
2023-11-07 12:12:50 +0800

    Fix ui bug for find
    Clears the current viewed company details
    when executing find.

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -146,7 +146,6 @@ public class ModelManager implements Model {
 
     @Override
     public void filterCompaniesByStatus(Predicate<Company> predicate) {
-        addressBook.clearDetailPanel();
         updateFilteredCompanyList(predicate);
     }
 
@@ -164,6 +163,7 @@ public class ModelManager implements Model {
     @Override
     public void updateFilteredCompanyList(Predicate<Company> predicate) {
         requireNonNull(predicate);
+        addressBook.clearDetailPanel();
         filteredCompanies.setPredicate(predicate);
     }
 
